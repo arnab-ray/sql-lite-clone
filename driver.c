e9b538c65b5032922909c1fc506016f130b1eb73
arnab-ray <arnab.ray@udaan.com>
2022-11-13 19:35:38 +0530

    initial commit

---
+++ driver.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+	char* buffer;
+	size_t buffer_length;
+	ssize_t input_length; 
+} InputBuffer;
+
+typedef enum {
+	META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum {
+	PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum {
+	STATEMENT_INSERT, STATEMENT_SELECT
+} StatementType;
+
+typedef struct {
+	StatementType type;
+} Statement;
+
+InputBuffer* create_input_buffer() {
+	InputBuffer* input_buffer = (InputBuffer*) malloc(sizeof(InputBuffer));
+	input_buffer->buffer = NULL;
+	input_buffer->buffer_length = 0;
+	input_buffer->input_length = 0;
+	return input_buffer; 
+}
+
+void print_prompt() {
+	printf("db> ");
+}
+
+void read_input(InputBuffer* input_buffer) {
+	ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+	
+	if (bytes_read <= 0) {
+		printf("Invalid input. \n");
+		exit(EXIT_FAILURE);
+	}
+
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+	free(input_buffer->buffer);
+	free(input_buffer);
+}
+
+MetaCommandResult run_meta_command(InputBuffer* input_buffer) {
+	if (strcmp(input_buffer->buffer, ".exit") == 0) {
+		exit(EXIT_SUCCESS);
+	} else {
+		return META_COMMAND_UNRECOGNIZED_COMMAND;
+	}
+}
+
+PrepareResult prepare_statement(InputBuffer* input_buffer, Statement* statement) {
+	if (strncmp(input_buffer->buffer, "insert", 6) == 0) {
+		statement->type = STATEMENT_INSERT;
+		return PREPARE_SUCCESS;
+	}
+	if (strcmp(input_buffer->buffer, "select") == 0) {
+		statement->type = STATEMENT_SELECT;
+		return PREPARE_SUCCESS;
+	}
+
+	return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement* statement) {
+	switch(statement->type) {
+		case(STATEMENT_INSERT):
+			printf("This is where we will do insert.\n");
+			break;
+		case(STATEMENT_SELECT):
+			printf("This is where we will do select.\n");
+			break;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	InputBuffer* input_buffer = create_input_buffer();
+	while(1) {
+		print_prompt();
+		read_input(input_buffer);
+
+		if (input_buffer->buffer[0] == '.') {
+			switch(run_meta_command(input_buffer)) {
+				case(META_COMMAND_SUCCESS):
+					continue;
+				case(META_COMMAND_UNRECOGNIZED_COMMAND):
+					printf("Unrecognized command '%s'\n", input_buffer->buffer);
+					continue;
+			}
+		}
+
+		Statement statement;
+		switch(prepare_statement(input_buffer, &statement)) {
+			case(PREPARE_SUCCESS):
+				break;
+			case(PREPARE_UNRECOGNIZED_STATEMENT):
+				printf("Unrecognized keyword at the start of '%s'. \n", input_buffer->buffer);
+				continue;
+		}
+		
+		execute_statement(&statement);
+		printf("Executed.\n");
+	}
+}
